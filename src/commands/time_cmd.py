1870bd613347c71e915e9c3b0ebeb6e15f859e18
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-04-18 14:02:51 +0200

    feat: Display exit hour for the current day. Refs #8

--- src/commands/time_cmd.py
+++ src/commands/time_cmd.py
@@ -1,5 +1,7 @@
+import datetime
 from lib.colors import *
 from lib.odoo_client import OdooClient
+from lib.time_functions import get_str_from_timesheet
 from argparse import _SubParsersAction, ArgumentParser, Namespace
 
 
@@ -15,5 +17,14 @@ class TimeCommand:
 
     def execute(self, _: Namespace):
         day_time, week_time = self.odoo_client.get_current_time()
+
+        hours, minutes = day_time.split("h")
+        
+        # Calculating the remaining time to work for a day of 7 hours.
+        delta_seconds = (3600 * 7) - datetime.timedelta(hours=int(hours), minutes=int(minutes)).seconds
+        delta = datetime.timedelta(seconds=delta_seconds)
+        exit_time = datetime.datetime.now() + delta
+
         print(bold("Journée actuelle :"), day_time)
         print(bold("Semaine actuelle :"), week_time)
+        print(bold("Fin de journée à :"), exit_time.strftime("%Hh%M"))
