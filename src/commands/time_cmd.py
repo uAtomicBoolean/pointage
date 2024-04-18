fa0efb880b4e05781f646d21820f3764f45114a4
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-04-18 14:20:28 +0200

    feat: Display overtime in time command. If the user is currently in overtime (for a wekk of 35h), then it will display the overtime next to the exit time.

--- src/commands/time_cmd.py
+++ src/commands/time_cmd.py
@@ -21,10 +21,27 @@ class TimeCommand:
         hours, minutes = day_time.split("h")
         
         # Calculating the remaining time to work for a day of 7 hours.
-        delta_seconds = (3600 * 7) - datetime.timedelta(hours=int(hours), minutes=int(minutes)).seconds
+        day_seconds = 3600 * 7
+        worked_seconds = datetime.timedelta(hours=int(hours), minutes=int(minutes)).seconds
+
+        overtime = 0
+        if day_seconds < worked_seconds:
+            overtime = worked_seconds - day_seconds
+            worked_seconds = day_seconds
+
+        delta_seconds = day_seconds - worked_seconds
         delta = datetime.timedelta(seconds=delta_seconds)
         exit_time = datetime.datetime.now() + delta
 
+        str_overtime = ""
+        if overtime:
+            minutes = overtime // 60
+            if minutes < 60:
+                str_overtime = f"(+{minutes}m)"
+            else:
+                str_overtime = f"(+{minutes // 60}h{minutes % 60})"
+
+
         print(bold("Journée actuelle :"), day_time)
         print(bold("Semaine actuelle :"), week_time)
-        print(bold("Fin de journée à :"), exit_time.strftime("%Hh%M"))
+        print(bold("Fin de journée à :"), exit_time.strftime("%Hh%M"), str_overtime)
