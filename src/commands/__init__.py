06c54e3851794928f207b60f57871ab50e96c66b
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2025-02-03 15:01:50 +0100

    Remove deprecated 'delete' command.

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -2,4 +2,3 @@ from .last_cmd import LastCommand
 from .time_cmd import TimeCommand
 from .presence_cmd import PresenceCommand
 from .update_cmd import UpdateCommand
-from .delete_cmd import DeleteCommand
